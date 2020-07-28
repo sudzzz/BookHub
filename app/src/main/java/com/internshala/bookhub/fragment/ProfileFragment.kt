c5c1e8943350eaa631efd6dc373086b23bdab2f2
Sudhir Daga <41622136+sudzzz@users.noreply.github.com>
2020-07-28 22:29:39 +0530

    first commit

---
+++ app/src/main/java/com/internshala/bookhub/fragment/ProfileFragment.kt
@@ -0,0 +1,20 @@
+package com.internshala.bookhub.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.internshala.bookhub.R
+
+class ProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/internshala/bookhub/util/ConnectionManager.kt
@@ -0,0 +1,25 @@
+package com.internshala.bookhub.util
+
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.core.content.getSystemService
+
+class ConnectionManager {
+    fun checkConnectivity(context: Context): Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+
+        if(activeNetwork?.isConnected!=null)
+        {
+            return activeNetwork.isConnected
+        }
+        else
+        {
+            return false
+        }
+
+    }
+}
